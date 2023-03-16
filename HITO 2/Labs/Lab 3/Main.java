f2f938a2c561934a006391e24cea614d9e0fb97a
NILBERMAYTA <110400517+NILBERMAYTA@users.noreply.github.com>
2023-03-16 14:03:04 +0100

    Add files via upload

---
+++ HITO 2/Labs/Lab 3/Main.java
@@ -0,0 +1,59 @@
+import Ejercicios.Math_utils;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //mostrando mensaje
+        //Ejercicios eje1 = new Ejercicios();
+
+        //eje1.mostrarMensaje();
+
+        //eje1.generarNumerosNatiralesFOR();
+
+        //eje1.generarNumerosNatiralesParesFOR();
+
+        //eje1.generarNumerosNatiralesParesWhile();
+
+        //System.out.println("Hola mundo desde Java");
+
+        Math_utils mu = new Math_utils("GOAPP","0.1",2022);
+        mu.printAPP();
+        mu.generateNaturalNumbers(5);
+
+
+
+
+
+
+        //ciclo for
+        //for (int i=1 ; i<=10 ; i++){
+        //    System.out.println(i);
+        //}
+
+        //concatenar
+        //for (int i=1 ; i<=10 ; i++){
+        //    System.out.print(i+ "  ");
+        //}
+        //System.out.println(" ");
+
+        //generar numeros pares
+        //for (int i=2 ; i<=20 ; i=i+2){
+        //    System.out.print(i+ "  ");
+        //}
+        //System.out.println(" ");
+
+        //uso while
+        //int i=1;
+        //while(i<=5){
+        //   System.out.print(i+ "  ");
+        //    i= i+1;
+        //}
+        //System.out.println(" ");
+
+        //i=2;
+        //while(i<=10){
+        //    System.out.print(i+ "  ");
+        //    i= i+2;
+        //}
+    }
+}
